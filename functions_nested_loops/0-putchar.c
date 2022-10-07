2e7aa63fd2d7b3f434dd1b863047050d61771543
Justino Velez <velezbalandran@gmail.com>
2022-10-07 13:54:07 -0500

    task 0 : putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar
+ * main - Prints Holberton
  * @void: Empty parameter list for main.
  *
- * Description: Prints _putchar w/out stdio
+ * Description: Prints Holberton w/out stdio
  *
  * Return: 0 for success
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
